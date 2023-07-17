7dbd1294c32db230796ee92e5854086faafb7393
yuusheng <hi@yuusheng.im>
2023-07-17 23:57:39 +0800

    chore: init

--- app/api/getNotion.ts
+++ app/api/getNotion.ts
@@ -3,15 +3,18 @@ import { Client } from '@notionhq/client'
 const databaseId = process.env.NOTION_DATABASE_ID!
 const notionKey = process.env.NOTION_KEY!
 
-export default async function Home() {
+export async function getNotion(req: any, res: any) {
   const notion = new Client({ auth: notionKey })
 
   const response = await notion.databases.query({
     database_id: databaseId,
+    filter: {
+      property: 'Status',
+      select: {
+        equals: 'Open',
+      },
+    },
   })
-  console.log(response.results)
-  return <>
-    <h1>Hi Next</h1>
-    <div>{JSON.stringify(response.results)}</div>
-  </>
+
+  res.status(200).json({ data: response.results })
 }
