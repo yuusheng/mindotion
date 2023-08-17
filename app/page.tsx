6e3aab1ef3879985ff27b83ac2f6995f165ae0d2
yuusheng <hi@yuusheng.im>
2023-08-17 22:36:05 +0800

    feat: setup shadcn/ui

--- app/page.tsx
+++ app/page.tsx
@@ -1,7 +1,12 @@
+import { Button } from '~/components/ui/button'
+
 export default function Home() {
   return (
     <>
       <h1>Mindotion</h1>
+      <Button>
+        click
+      </Button>
     </>
   )
 }
